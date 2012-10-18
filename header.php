c0258c27ce77fed91dc1ac1ae7ac3063daf6f604
Chris Rock <ckrock@gmail.com>
2012-10-18 12:14:39 -0600

    HTML 5 doctype

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!doctype html>
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes() ?>>
 <head>
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
