7cb09e37e4eaa683548d31582276a744c20451e4
Roy Hayward <roy@Centaurus.local>
2013-09-23 09:45:51 -0600

    adding support declarations for Woo-Commerce

--- functions.php
+++ functions.php
@@ -6,3 +6,21 @@ register_sidebar(array(
     'before_title' => '<h3 class="widgettitle">',
     'after_title' => '</h3>',
 ));
+/***************
+ * Woo Theme support Declaration
+ ***************/
+add_theme_support( 'woocommerce' );
+//unhookd woocommerce wrappers
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+//Then hook in your own functions to display the wrappers your theme requires;
+add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+
+function my_theme_wrapper_start() {
+  echo '<section id="main">';
+}
+
+function my_theme_wrapper_end() {
+  echo '</section>';
+}
