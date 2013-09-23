97aecfd408c66c177d8319c1a33f53fb57cbc27d
Roy Hayward <roy@theintegrationengineer.com>
2013-09-23 11:00:59 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -14,13 +14,13 @@ add_theme_support( 'woocommerce' );
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
 remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
 //Then hook in your own functions to display the wrappers your theme requires;
-add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
-add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+add_action('woocommerce_before_main_content', 'ek_theme_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'ek_theme_wrapper_end', 10);
 
-function my_theme_wrapper_start() {
+function ek_theme_wrapper_start() {
   echo '<section class="main">';
 }
 
-function my_theme_wrapper_end() {
+function ek_theme_wrapper_end() {
   echo '</section>';
 }
