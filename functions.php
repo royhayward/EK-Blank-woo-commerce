0cbc27ec12e24c077b8025605226e36edc2893df
Roy Hayward <roy@theintegrationengineer.com>
2013-09-23 10:29:22 -0600

    Update functions.php
    changing to class from ID on wrapper start to pick up the style correctly.

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
 add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
 
 function my_theme_wrapper_start() {
-  echo '<section id="main">';
+  echo '<section class="main">';
 }
 
 function my_theme_wrapper_end() {
